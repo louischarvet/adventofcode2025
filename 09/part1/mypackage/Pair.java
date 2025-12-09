d1bb9018a99364cca160d515c00b6be48f2679f3
lucius <lucius@SIVA.localdomain>
2025-12-09 13:18:57 +0100

    09 part1

---
+++ 09/part1/mypackage/Pair.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class Pair< F, S > {
+	private final F	first;
+	private final S	second;
+
+	public Pair(F first, S second) {
+		this.first = first;
+		this.second = second;
+	}
+
+	public F	getFirst() { return first; }
+	public S	getSecond() { return second; }
+}
